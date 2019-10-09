e039fe20998d31a121c05dcc94d332ed10e943dd
Steven Huang <steven.charles.huang@gmail.com>
2019-10-09 15:56:58 -0400

    fixed typo

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -58,7 +58,7 @@ SELECT city.name, country.name
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
  -- follow right behind you!
-SELECT city.name AS capital, country.name AS ountry
+SELECT city.name AS capital, country.name AS country
   FROM country
   JOIN city
     ON country.code = city.countrycode
