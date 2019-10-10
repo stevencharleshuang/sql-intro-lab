8c2bc383e1028f34db8e2107b5baec351514eed5
Steven Huang <steven.charles.huang@gmail.com>
2019-10-09 21:20:22 -0400

    fixed minor format issue

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -1,11 +1,11 @@
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been
 -- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
-  SELECT name 
-    FROM country 
-   WHERE region = 'Southern Europe' 
-ORDER BY population 
-   LIMIT 1;
+SELECT name 
+  FROM country 
+ WHERE region = 'Southern Europe' 
+ ORDER BY population 
+ LIMIT 1;
 --> Holy See (Vatican City State)
 
 
